5f39b55630a0295f87c432f3706fc7c812d2e7a9
nicksnikose <125879233+nicksnikose@users.noreply.github.com>
2023-06-22 16:34:56 +0530

    new commit
    check it

---
+++ Aboutus.jsx
@@ -0,0 +1,17 @@
+import Middle from "../components/Middle";
+import Footer from "../components/Fotter";
+
+const Aboutus = ()=>{
+    return(
+        <div>
+            <div>
+
+            <Middle/>
+            </div>
+            <div>
+                <Footer/>
+            </div>
+        </div>
+    )
+}
+export default Aboutus 
